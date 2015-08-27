10002d7419f0e3b7ccbbb8ebd5424b320854bea8
moytho <moytho@hotmail.com>
2015-08-26 22:00:56 -0600

    Primer git

---
+++ AspNetIdentity.WebApi/Models/Clinica.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AspNetIdentity.WebApi.Models
+{
+    public class Clinica
+    {
+        [Key]
+        public int Clinica { get; set; }
+        public string Nombre{get;set;}
+        public string Telefono { get; set; }
+        public string Direccion { get; set; }
+
+
+    }
+}
